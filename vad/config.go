6570c6dfff33e2bc96be198781366fd64ddac348
henryleu <henryleu@126.com>
2020-06-02 18:18:53 +0800

    update

--- vad/config.go
+++ vad/config.go
@@ -4,24 +4,27 @@ import (
 	"fmt"
 )
 
-// Config loads config from json cofig file.
+// Config loads config from json config file.
 type Config struct {
 	// SpeechTimeout is period of activity required to complete transition
 	// to active state. By default, 300 (ms)
-	SpeechTimeout int
+	SpeechTimeout int ``
 
 	// SilenceTimeout is period of inactivity required to complete transition
 	// to inactive state. By default, 300 (ms)
 	SilenceTimeout int
 
-	// NonputTimeout is no input timeout. By default, 5000 (ms)
-	NonputTimeout int
+	// NoinputTimeout is no input timeout. By default, 5000 (ms)
+	NoinputTimeout int
+
+	// NoinputTimers is a flag indicates if timer is on. By default, true
+	NoinputTimers bool
 
 	// RecognitionTimeout is recognition timeout. By default, 20000 (ms)
 	RecognitionTimeout int
 
-	// VADMode is the aggressiveness mode for vad. By default, 3 for anti background noise
-	VADMode Mode
+	// VADLevel is the aggressiveness mode for vad. By default, 3 for anti background noise
+	VADLevel VADLevel
 
 	// SampleRate defines the number of samples per second, aka. sample rate.
 	// It only supports 8000 and 16000.
@@ -35,15 +38,20 @@ type Config struct {
 
 	// FrameTime defines Codec frame time in msec. It should be 10ms, 20ms or 30ms. By default, 20 (ms).
 	FrameTime int
+
+	// Multiple means if the detector is used to detect multiple speeches.
+	// true is for processing a record wave file.
+	// false is for processing a incoming voice stream.
+	Multiple bool
 }
 
-// Mode is the aggressiveness mode for vad and there are only 4 modes supported.
+// VADLevel is the aggressiveness level for vad and there are only 4 modes supported.
 // 0: vad normal;
 // 1: vad low bitrate;
 // 2: vad aggressive;
 // 3: vad very aggressive;
 // By default, 3 is used because it is good at anti background noise.
-type Mode int
+type VADLevel int
 
 const (
 
@@ -91,36 +99,68 @@ const (
 var defaultConfig = Config{
 	SpeechTimeout:      300,
 	SilenceTimeout:     300,
-	NonputTimeout:      5000,
+	NoinputTimeout:     5000,
+	NoinputTimers:      true,
 	RecognitionTimeout: 20000,
-	VADMode:            VADVeryAggressive,
+	VADLevel:           VADVeryAggressive,
 	SampleRate:         SampleRate8,
 	BytesPerSample:     BytesPerSample,
 	BitsPerSample:      BitsPerSample,
-	FrameTime:          FrameTimeBase * 2,
+	FrameTime:          FrameTime20,
+	Multiple:           false,
 }
 
-// LoadConfig loads config from json file.
-func LoadConfig(path string) *Config {
-	config := defaultConfig
-	return &config
-}
-
-func (c *Config) init() error {
-	if c.VADMode != VADNormal && c.VADMode != VADLowBitrate && c.VADMode != VADAggressive && c.VADMode != VADVeryAggressive {
+// Validate checks the validity of the config
+func (c *Config) Validate() error {
+	if c.SpeechTimeout <= 0 {
 		// todo logging and wrap error
-		return fmt.Errorf("Detector.VADMode should be 0, 1, 2 or 3, got %c", c.VADMode)
+		return fmt.Errorf("Detector.SpeechTimeout should be greater than 0, got %v", c.SpeechTimeout)
+	}
+
+	if c.SilenceTimeout <= 0 {
+		// todo logging and wrap error
+		return fmt.Errorf("Detector.SilenceTimeout should be greater than 0, got %v", c.SilenceTimeout)
+	}
+
+	if c.NoinputTimeout <= 0 {
+		// todo logging and wrap error
+		return fmt.Errorf("Detector.NoinputTimeout should be greater than 0, got %v", c.NoinputTimeout)
+	}
+
+	if c.RecognitionTimeout <= 0 {
+		// todo logging and wrap error
+		return fmt.Errorf("Detector.RecognitionTimeout should be greater than 0, got %v", c.RecognitionTimeout)
+	}
+
+	if c.VADLevel != VADNormal && c.VADLevel != VADLowBitrate && c.VADLevel != VADAggressive && c.VADLevel != VADVeryAggressive {
+		// todo logging and wrap error
+		return fmt.Errorf("Detector.VADLevel should be 0, 1, 2 or 3, got %v", c.VADLevel)
 	}
 
 	if c.SampleRate != SampleRate8 && c.SampleRate != SampleRate16 {
 		// todo logging and wrap error
-		return fmt.Errorf("Detector.SampleRate should be 8000 or 16000, got %c", c.SampleRate)
+		return fmt.Errorf("Detector.SampleRate should be 8000 or 16000, got %v", c.SampleRate)
+	}
+
+	if c.BytesPerSample != BytesPerSample {
+		// todo logging and wrap error
+		return fmt.Errorf("Detector.BytesPerSample should be 2, got %v", c.BytesPerSample)
+	}
+
+	if c.BitsPerSample != BitsPerSample {
+		// todo logging and wrap error
+		return fmt.Errorf("Detector.BitsPerSample should be 16, got %v", c.BitsPerSample)
 	}
 
 	if c.FrameTime != FrameTime10 && c.FrameTime != FrameTime20 && c.FrameTime != FrameTime30 {
 		// todo logging and wrap error
-		return fmt.Errorf("Detector.FrameTime should be 10, 20 or 30, got %c", c.FrameTime)
+		return fmt.Errorf("Detector.FrameTime should be 10, 20 or 30, got %v", c.FrameTime)
 	}
 
 	return nil
+}
+
+// NewDetector creates a detector with the config populated.
+func (c *Config) NewDetector() *Detector {
+	return NewDetector(c)
 }
--- vad/detector.go
+++ vad/detector.go
@@ -1,7 +1,9 @@
 package vad
 
 import (
+	"errors"
 	"fmt"
+	"log"
 
 	webrtcvad "github.com/maxhawkins/go-webrtcvad"
 )
@@ -40,92 +42,74 @@ const (
 	EventNoInput
 )
 
+const (
+	topVolumeValve = 0.8
+
+	bottomVolumeValve = 0.2
+
+	cacheSecond = 16000 // frame cache for one second
+
+	cacheCap = cacheSecond * 10 // frame cache for 10 seconds
+)
+
 // Detector detects voice from voice stream based on FSM (finite state machine)
 // and VAD library ported from WebRTC
 type Detector struct {
+	// Config contains the all the parameters for tuning and controling the detector's behaviors
+	*Config
+
 	// State is the state of the detector. By default, StateInactivity.
 	State State
-
-	// LevelThreshold is the level threshold of voice activity (silence).
-	// It should be in [0 .. 255]
-	// LevelThreshold int
-
-	// SpeechTimeout is period of activity required to complete transition
-	// to active state. By default, 300 (ms)
-	SpeechTimeout int
-
-	// SilenceTimeout is period of inactivity required to complete transition
-	// to inactive state. By default, 300 (ms)
-	SilenceTimeout int
 
 	// Duration is the duration spent in current state. By default, 0
 	Duration int
 
-	// NonputTimeout is no input timeout. By default, 5000 (ms)
-	NonputTimeout int
-
-	// NoInputDuration is the duration spent during no input state (inactivity state).
+	// NoinputDuration is the duration spent during no input state (inactivity state).
 	// By default, 0 (ms)
-	NoInputDuration int
-
-	// RecognitionTimeout is recognition timeout. By default, 20000 (ms)
-	RecognitionTimeout int
+	NoinputDuration int
 
 	// RecognitionDuration is the duration spent during activity and inactivity transition state.
 	// By default, 0 (ms)
 	RecognitionDuration int
 
-	// VADMode is the aggressiveness mode for vad. By default, 3 for anti background noise
-	VADMode Mode
-
-	// TimersStarted is a flag indicates if timer is on. By default, true
-	// TimersStarted bool
-
-	// SampleRate defines the number of samples per second, aka. sample rate.
-	// It only supports 8000 and 16000.
-	SampleRate int
-
-	// BytesPerSample defines bytes per sample for linear pcm
-	BytesPerSample int
-
-	// BitsPerSample defines bits per sample for linear pcm
-	BitsPerSample int
-
-	// FrameTime defines Codec frame time in msec. It should be 10ms, 20ms or 30ms. By default, 20 (ms).
-	FrameTime int
-
 	// vad is WebRTC VAD processor
 	vad *webrtcvad.VAD
 
-	sampleCount    int
-	vadSampleCount int
-	// speechDuration int
+	sampleCount, vadSampleCount int
 
-	debug bool
-	work  bool
+	speechStart, speechEnd int
+
+	// bytes per millisecond based sample rate and sample depth (bytes per sample)
+	bytesPerMillisecond int
+
+	// work indicates if the detector's work is over.
+	// true is for working.
+	// false is for over.
+	work bool
+
+	// frame cache for all incoming samples
+	cache []byte
 }
 
 // DefaultDetector is
 var defaultDetector = Detector{
 	State:               StateInactivity,
-	SpeechTimeout:       300,
-	SilenceTimeout:      300,
-	NonputTimeout:       5000,
 	Duration:            0,
-	NoInputDuration:     0,
-	RecognitionTimeout:  20000,
+	NoinputDuration:     0,
 	RecognitionDuration: 0,
-	VADMode:             VADVeryAggressive,
-	SampleRate:          SampleRate8,
-	BytesPerSample:      BytesPerSample,
-	BitsPerSample:       BitsPerSample,
-	FrameTime:           FrameTimeBase * 2,
+	sampleCount:         0,
+	vadSampleCount:      0,
+	speechStart:         0,
+	speechEnd:           0,
+	work:                true,
 }
 
 // NewDetector creates
-func NewDetector() *Detector {
-	detector := defaultDetector
-	return &detector
+func NewDetector(config *Config) *Detector {
+	d := defaultDetector
+	d.Config = config
+	d.cache = make([]byte, 0, cacheCap)
+	return &d
 }
 
 // Init initiates vad and check configuration
@@ -136,27 +120,238 @@ func (d *Detector) Init() error {
 		return err
 	}
 
-	if d.VADMode != VADNormal && d.VADMode != VADLowBitrate && d.VADMode != VADAggressive && d.VADMode != VADVeryAggressive {
-		// todo logging and wrap error
-		return fmt.Errorf("Detector.VADMode should be 0, 1, 2 or 3, got %d", d.VADMode)
-	}
-
-	err = vad.SetMode(int(d.VADMode))
+	err = vad.SetMode(int(d.VADLevel))
 	if err != nil {
 		// todo logging and wrap error
 		return err
 	}
 	d.vad = vad
 
-	if d.SampleRate != SampleRate8 && d.SampleRate != SampleRate16 {
-		// todo logging and wrap error
-		return fmt.Errorf("Detector.SampleRate should be 8000 or 16000, got %d", d.SampleRate)
-	}
+	d.bytesPerMillisecond = d.SampleRate * d.BitsPerSample / 1000
 
-	if d.FrameTime != FrameTime10 && d.FrameTime != FrameTime20 && d.FrameTime != FrameTime30 {
-		// todo logging and wrap error
-		return fmt.Errorf("Detector.FrameTime should be 10, 20 or 30, got %d", d.FrameTime)
-	}
-
+	// todo init other resources
 	return nil
+}
+
+func (d *Detector) setState(state State) {
+	d.State = state
+	d.Duration = 0
+}
+
+func (d *Detector) cacheFrame(frame []byte) {
+	d.cache = append(d.cache, frame...)
+}
+
+// Process process the frame of incoming voice samples and generate detection event
+func (d *Detector) Process(frame []byte) error {
+	// check if the detector is still working
+	if d.Multiple && !d.work {
+		return nil
+	}
+
+	// todo validate frame
+
+	// calc real times in the frame
+	frameTime := len(frame) / d.bytesPerMillisecond
+
+	d.cacheFrame(frame)
+
+	result, err := d.vad.Process(d.SampleRate, frame)
+	if err != nil {
+		msg := fmt.Sprintf("Fail to vad process - %v", err)
+		log.Println(msg)
+		return errors.New(msg)
+	}
+
+	// todo result and volume level checking
+
+	// check recognition timeout
+	if d.State == StateActivity || d.State == StateInactivityTransition {
+		d.RecognitionDuration += frameTime
+		if d.RecognitionDuration >= d.RecognitionTimeout {
+			d.setState(StateInactivity)
+			d.RecognitionDuration = 0
+			// todo emit event(inactivity)
+			return nil
+		}
+	}
+
+	switch d.State {
+	case StateInactivity:
+		if result {
+			// start to detect activity
+			d.sampleCount = 0
+			d.vadSampleCount = 0
+			d.setState(StateActivityTransition)
+		} else {
+			if d.NoinputTimers {
+				d.Duration += frameTime
+				d.NoinputDuration += frameTime
+				if d.NoinputDuration >= d.NoinputTimeout {
+					// detected noinput
+					// todo emit event(noinput)
+					d.NoinputDuration = 0
+				}
+			}
+		}
+		break
+	case StateActivityTransition:
+		d.sampleCount++
+		if result {
+			d.vadSampleCount++
+		}
+		if result || float32(d.vadSampleCount/d.sampleCount) > topVolumeValve {
+			d.Duration += frameTime
+			if d.Duration >= d.SpeechTimeout {
+				// finally detected activity
+				// d.speechDuration = d.Duration
+				// todo record speech
+				d.setState(StateActivity)
+				// todo emit event(activity)
+			}
+		} else {
+			// fallback to inactivity
+			d.NoinputDuration += frameTime
+			d.setState(StateInactivity)
+		}
+		break
+	case StateActivity:
+		if result {
+			d.Duration += frameTime
+		} else {
+			// start to detect inactivity
+			d.sampleCount = 0
+			d.vadSampleCount = 0
+			d.setState(StateInactivityTransition)
+		}
+		break
+	case StateInactivityTransition:
+		d.sampleCount++
+		if result {
+			d.vadSampleCount++
+		}
+		if result && float32(d.vadSampleCount/d.sampleCount) > bottomVolumeValve {
+			// fallback to activity
+			d.setState(StateActivity)
+		} else {
+			d.Duration += frameTime
+			if d.Duration >= d.SilenceTimeout {
+				// detected inactivity
+				if !d.Multiple {
+					d.work = false
+				}
+				d.setState(StateInactivity)
+				// todo emit event(inactivity)
+			}
+		}
+
+		break
+	}
+	return nil
+	/*
+
+		  mpf_detector_event_e det_event = MPF_DETECTOR_EVENT_NONE;
+		  if (detector->work == FALSE) {
+		    return det_event;
+		  }
+
+		  apt_bool_t result = FALSE;
+		  int time_base = CODEC_FRAME_TIME_BASE;
+		  apr_size_t level = mpf_activity_detector_level_calculate(frame);
+		  int vad = WebRtcVad_Process(detector->vad_inst,
+		                              detector->voice_rate,
+		                              frame->codec_frame.buffer,
+		                              frame->codec_frame.size / 2,
+		                              1);
+
+		  if (level >= detector->level_threshold && vad == 1) {
+		    result = TRUE;
+		  }
+
+		  if (detector->state == DETECTOR_STATE_ACTIVITY || detector->state == DETECTOR_STATE_INACTIVITY_TRANSITION) {
+		    detector->recognition_duration += time_base;
+		    if (detector->recognition_duration >= detector->recognition_timeout) {
+		      mpf_activity_detector_state_change(detector, DETECTOR_STATE_INACTIVITY);
+		      detector->recognition_duration = 0;
+		      return MPF_DETECTOR_EVENT_INACTIVITY;
+		    }
+		  }
+
+			if (detector->debug == TRUE) {
+		    printf("voice info: level=%5d, google=%d, mrcp=%d, state=%d, duration=%dms\n",
+		           (int) level,
+		           vad,
+		           result,
+		           detector->state,
+		           (int) detector->recognition_duration);
+		  }
+	*/
+
+	/*
+	  if (detector->state == DETECTOR_STATE_INACTIVITY) {
+	    if (result == TRUE) {
+	      // start to detect activity
+	      detector->sample_count = 0;
+	      detector->vad_sample_count = 0;
+	      mpf_activity_detector_state_change(detector, DETECTOR_STATE_ACTIVITY_TRANSITION);
+	    } else {
+	      if (detector->timers_started == TRUE) {
+	        detector->duration += time_base;
+	        detector->noinput_duration += time_base;
+	        if (detector->noinput_duration >= detector->noinput_timeout) {
+	          // detected noinput
+	          det_event = MPF_DETECTOR_EVENT_NOINPUT;
+	          detector->noinput_duration = 0;
+	        }
+	      }
+	    }
+	  } else if (detector->state == DETECTOR_STATE_ACTIVITY_TRANSITION) {
+	    detector->sample_count++;
+	    if (result == TRUE) {
+	      detector->vad_sample_count++;
+	    }
+	    if (result == TRUE || (float) detector->vad_sample_count / detector->sample_count > 0.8) {
+	      detector->duration += time_base;
+	      if (detector->duration >= detector->speech_timeout) {
+	        // finally detected activity
+	        detector->speech_duration = detector->duration;
+	        det_event = MPF_DETECTOR_EVENT_ACTIVITY;
+	        mpf_activity_detector_state_change(detector, DETECTOR_STATE_ACTIVITY);
+	      }
+	    } else {
+	      // fallback to inactivity
+	      detector->noinput_duration += time_base;
+	      mpf_activity_detector_state_change(detector, DETECTOR_STATE_INACTIVITY);
+	    }
+	  } else if (detector->state == DETECTOR_STATE_ACTIVITY) {
+	    if (result == TRUE) {
+	      detector->duration += time_base;
+	    } else {
+	      // start to detect inactivity
+	      detector->sample_count = 0;
+	      detector->vad_sample_count = 0;
+	      mpf_activity_detector_state_change(detector, DETECTOR_STATE_INACTIVITY_TRANSITION);
+	    }
+	  } else if (detector->state == DETECTOR_STATE_INACTIVITY_TRANSITION) {
+	    detector->sample_count++;
+	    if (result == TRUE) {
+	      detector->vad_sample_count++;
+	    }
+	    if (result == TRUE && (float) detector->vad_sample_count / detector->sample_count > 0.2) {
+	      // fallback to activity
+	      mpf_activity_detector_state_change(detector, DETECTOR_STATE_ACTIVITY);
+	    } else {
+	      detector->duration += time_base;
+	      if (detector->duration >= detector->silence_timeout) {
+	        // detected inactivity
+	        det_event = MPF_DETECTOR_EVENT_INACTIVITY;
+	        detector->work = FALSE;
+	        mpf_activity_detector_state_change(detector, DETECTOR_STATE_INACTIVITY);
+	      }
+	    }
+	  }
+
+	  return det_event;
+
+	*/
 }
